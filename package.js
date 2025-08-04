35dd672c327c00f303b4ca87a33822d5fc63033d
khanbhai009-cloud <khanbhai9236716296@gmail.com>
2025-08-04 10:22:19 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "fashion-empire",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "export": "next export"
+  },
+  "dependencies": {
+    "next": "13.4.9",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  }
+}
